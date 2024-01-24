9768599475b2c70c07f0bf8167787b753f760ba8
JjayFabor <faborjaylordvhan@gmail.com>
2024-01-25 00:13:30 +0800

    Updated Buttons

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 from flet import *
 
+def switch_page(page: Page):
+    pass
+
 def main(page: Page):
 
     BG = '#041955'
     FWG = '#97b4ff'
     FG = '#3450a1'
 
-    cash_in = Row(
+    buttons = Column(
         controls=[
             FilledButton(text='Cash In', width=125, height=50),
             FilledButton(text='Cash Out', width=125, height=50),
+            FilledButton(text='View', width=125, height=50),
         ],
         alignment=MainAxisAlignment.CENTER,
     )
     container = Container(
-        width=400,
-        height=850, 
+        width='90vw',
+        height=750, 
         bgcolor=BG,
         border_radius=35,
         alignment=alignment.center,
-
-        content=Stack(
-            controls=[
-                cash_in,
-            ],
+        content = Container(
+            Container(buttons),
         )
     )
 
